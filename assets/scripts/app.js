a39ac31cd6d31a5226655d17d6606f5516d0c8bf
RiverMagnetic <magneticcontinuum@gmail.com>
2018-08-29 21:41:26 -0400

    Require and utilize authEvents file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict'
-
+const authEvents = require('./auth/authEvents')
+// const journalEntryEvents = require('./journal-entry/journalEntryEvents')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +8,6 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  authEvents.addHandlers()
+  // journalEntryEvents.addHandlers()
 })
