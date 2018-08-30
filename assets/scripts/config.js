2c7414bb8a34ef791ca222315c5ca0ffedc0f561
RiverMagnetic <magneticcontinuum@gmail.com>
2018-08-29 21:39:59 -0400

    Add heroku URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://api-rails-just-add-water.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
