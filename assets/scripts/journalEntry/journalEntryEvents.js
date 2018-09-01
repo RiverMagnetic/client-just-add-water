c88e455467ef58cbcf1ab6ceef769c2f1effb398
RiverMagnetic <magneticcontinuum@gmail.com>
2018-08-31 20:40:10 -0400

    Add working code & potential updateJournalEntry
    Added working GET Journal Entries event handler and callback function, enabled UI for CREATE journal entries by uncommenting code, added commented-out UPDATE journal entries code for potential future use.

--- assets/scripts/journalEntry/journalEntryEvents.js
+++ assets/scripts/journalEntry/journalEntryEvents.js
@@ -10,11 +10,33 @@ const onCreateJournalEntry = function (event) {
     // could put some input validation here
     // message the user if validation fails
     journalEntryApi.createJournalEntry(data)
-      // .then(journalEntryUi.onCreateJournalEntrySuccess)
+      .then(journalEntryUi.onCreateJournalEntrySuccess)
     //   .then(() => $('#create-journal-entry-content').trigger('reset'))
-      // .catch(journalEntryUi.failure)
+      .catch(journalEntryUi.onError)
     $('#create-journal-entry').trigger('reset')
   }
+
+  // const onUpdateJournalEntry = function (event) {
+  //   event.preventDefault()
+  //   console.log('running onUpdateJournalEntry')
+  //   const data = getFormFields(event.target)
+  //   const journalEntry = data.journalEntry
+  //   if (journalEntry.id.length !== 0) {
+  //       // console.log("the length of the journal entry id is", journalEntry.id.length)
+  //       // console.log("we're in onUpdateJournalEntry and the data is:", data)
+  //       journalEntryApi.updateJournalEntry(data)
+  //           // .then(journalEntryUi.onUpdateJournalEntrySuccess)
+  //           // .then(() => onClearJournalEntries(event))
+  //           // .then(() => onGetJournalEntries(event))
+  //           // .catch(journalEntryUi.onError)
+  //   } else {
+  //       // $('#message').html('Please provide an journalEntry id!')
+  //       // $('#message').css('background-color', 'red')
+  //       console.log('Please provide an journalEntry id!')
+  //   }
+  //   $('#update-journal-entry').trigger('reset')
+  //   //  the native way to do this would be: document.querySelector('#update-journal-entry').reset()
+// }
 
   const addHandlers = () => {
     // Between the sets of single quotes,
@@ -22,8 +44,9 @@ const onCreateJournalEntry = function (event) {
 
     // optional: add a selector and/or data before the event/s:
  
-  
+    $('#get-journal-entries').on('click', onGetJournalEntries)
     $('#create-journal-entry').on('submit', onCreateJournalEntry)
+    // $('#update-journal-entry').on('submit', onUpdateJournalEntry)
   }  
 
 module.exports = {
