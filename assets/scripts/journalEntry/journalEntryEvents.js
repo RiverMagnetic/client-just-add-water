168fab469ac0c998636dcd222d757f0c228ac80e
RiverMagnetic <magneticcontinuum@gmail.com>
2018-09-05 14:20:21 -0400

    Fill CREATE form for UPDATE, experiment
    Added working function to fill the CREATE form with a previous journal entry, changed attribute for onDeleteJournalEntry to reflect change in handlebars file, experimenting with CREATE, commented out code to isolate experiments.

--- assets/scripts/journalEntry/journalEntryEvents.js
+++ assets/scripts/journalEntry/journalEntryEvents.js
@@ -3,6 +3,7 @@
 const getFormFields = require('../../../lib/get-form-fields')
 const journalEntryApi = require('./journalEntryApi.js')
 const journalEntryUi = require('./journalEntryUi.js')
+const store = require('../store')
 
 const onClearJournalEntries = (event) => {
   event.preventDefault()
@@ -15,23 +16,55 @@ const onGetAllJournalEntries = (event) => {
       .then(journalEntryApi.getAllJournalEntries)
       .then(journalEntryUi.getAllJournalEntriesSuccess)
       .catch(journalEntryUi.onError)
+      console.log('onGetAllJournalEntries')
 }
 
+// const showCreateJournalEntryForm = function (event) {
+  
+//   $('#create-journal-entry').show()
+// }
+
 const onCreateJournalEntry = function (event) {
-    event.preventDefault()
-    const data = getFormFields(event.target)
-    // could put some input validation here
-    // message the user if validation fails
-    journalEntryApi.createJournalEntry(data)
-      .then(journalEntryUi.onCreateJournalEntrySuccess)
-    //   .then(() => $('#create-journal-entry-content').trigger('reset'))
-      .catch(journalEntryUi.onError)
-    $('#create-journal-entry').trigger('reset')
+  event.preventDefault()
+  console.log('onCreateJournalEntry')
+  const data = getFormFields(event.target)
+//     // could put some input validation here
+//     // message the user if validation fails
+//     // journalEntryApi.createJournalEntry(data)
+//       // .then(journalEntryUi.onCreateJournalEntrySuccess)
+//       // .then(() => $('#create-journal-entry-content').trigger('reset'))
+//       // .catch(journalEntryUi.onError)
+//     // $('#create-journal-entry').trigger('reset')
   }
 
-  const onUpdateJournalEntry = function (event) {
-    event.preventDefault()
-    console.log('running onUpdateJournalEntry')
+  const previousJournalEntry = (event) => {
+    store.journal_entry = {
+      id: $(event.target).closest('section').attr('data-journal-entry-id'),
+      created_at: $(event.target).closest('section').attr('data-journal-entry-created-at'),
+      journal_entry_content: $(event.target).closest('section').attr('data-journal-entry-content')
+    }
+    console.log(store.journal_entry)
+    $('#previous-entries').hide()
+    fillWithPreviousJournalEntry()
+    console.log(store.journal_entry)
+    console.log('previousJournalEntry ran.')
+  }
+  
+  // fillWithPreviousJournalEntry fills the textarea of the create-journal-entry form with the closest journal_entry_content on 'edit' button click
+  const fillWithPreviousJournalEntry = () => {
+    $('#journal-entry').val(store.journal_entry.journal_entry_content)
+    console.log('jounalentry content is', store.journal_entry.journal_entry_content)
+    // the following line is psuedocode for an idea for handling UI for this feature
+    //   $('#data-journal-entry-content').hide()
+      $('#create-journal-entry').show()
+      $('#create-journal-entry-button').hide()
+      // $('#update-journal-entry-button').show()
+    console.log('fillWithPreviousJournalEntry ran.')
+  }
+
+  // const onUpdateJournalEntry = function (event) {
+  //   event.preventDefault()
+  //   console.log('running onUpdateJournalEntry')
   //   const data = getFormFields(event.target)
   //   const journalEntry = data.journalEntry
   //   if (journalEntry.id.length !== 0) {
@@ -49,11 +82,12 @@ const onCreateJournalEntry = function (event) {
   //   }
   //   $('#update-journal-entry').trigger('reset')
     //  the native way to do this would be: document.querySelector('#update-journal-entry').reset()
-}
+// }
 const onDeleteJournalEntry = (event) => {
   console.log('hey,this is onDeleteJournalEntry')
   event.preventDefault()
-  const journalEntryId = $(event.target).closest('section').attr('data-id')
+  console.log($(event.target))
+  const journalEntryId = $(event.target).closest('section').attr('data-journal-entry-id')
   journalEntryApi.deleteJournalEntry(journalEntryId)
       // may need refactoring
       // .then(journalEntryUi.onDeleteJournalEntrySuccess)
@@ -66,12 +100,15 @@ const onDeleteJournalEntry = (event) => {
     // add the HTML id, class, or element to attach an event to, and the event/s
 
     // optional: add a selector and/or data before the event/s:
- 
+    // $('#show-create-journal-entry').on('click', showCreateJournalEntryForm)
     $('#get-all-journal-entries').on('click', onGetAllJournalEntries)
-    $('#create-journal-entry').on('submit', onCreateJournalEntry)
-    // $('.previous-entries').on('submit', onUpdateJournalEntry)
-    // $('.previous-entries').on('click', 'button', onJournalEntry)
-    $('.previous-entries').on('click', 'button', onDeleteJournalEntry)
+    // $('#get-all-journal-entries').on('click', console.log('onGetAllJournalEntries'))
+    // $('#create-journal-entry').on('submit', '#create-journal-entry-button', onCreateJournalEntry)
+    $('#create-journal-entry-button').on('submit', onCreateJournalEntry)
+    // // $('#update-journal-entry-button').on('submit', onUpdateJournalEntry)
+    // $('#update-journal-entry-button').on('submit', console.log('onUpdateJournalEntry'))
+    $('.previous-entries').on('click', '#edit-journal-entry-button', previousJournalEntry)
+    $('.previous-entries').on('click', '#erase-journal-entry-button', onDeleteJournalEntry)
   }  
 
 module.exports = {
