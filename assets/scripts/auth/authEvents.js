8d119560e768ad59fb75b9fe73743372a2ead14d
RiverMagnetic <magneticcontinuum@gmail.com>
2018-09-14 15:19:53 -0400

    Move reset on signUp to reduce code duplication

--- assets/scripts/auth/authEvents.js
+++ assets/scripts/auth/authEvents.js
@@ -23,6 +23,7 @@ const onSignUp = function (event) {
     authApi.signUp(data)
         .then(authUi.signUpSuccess)
         .catch(authUi.signUpFailure)
+    $('#sign-up').trigger('reset')
 }
 
 const onSignIn = function (event) {
